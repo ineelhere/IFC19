bef6342f11f736b916bba35b8ba4e661c212ad4b
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-08-15 02:48:41 +0530

    Update ifc19_app.py

--- 2.0/ifc19_app.py
+++ 2.0/ifc19_app.py
@@ -4,7 +4,6 @@ st.markdown("![](https://komarev.com/ghpvc/?username=ineelhere&color=blue)")
 st.title("India Fights COVID19 (IFC19)")
 st.markdown("""
 webapp version 2.0 | development under progress
-
 ![](https://miro.medium.com/max/1400/1*CsJ05WEGfunYMLGfsT2sXA.gif)
 """)
 df = pd.read_csv("https://api.covid19india.org/csv/latest/state_wise.csv")
@@ -12,7 +11,6 @@ st.write(f"State wise data")
 st.write(df)
 
 st.markdown("""
-<hr>
-
-**© Indraneel Chakraborty | 2021**
+___
+**© [Indraneel Chakraborty](https://www.linkedin.com/in/indraneelchakraborty/) | 2021**
 """)
