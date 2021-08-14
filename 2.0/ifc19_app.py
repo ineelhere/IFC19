695cfe1289c6bc8870e325d9584d38fc040859a8
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-08-15 02:36:31 +0530

    Create ifc19_app.py

---
+++ 2.0/ifc19_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.title("India Fights COVID19 (IFC19)")
+st.markdown("""
+webapp version 2.0 | development under progress
+
+![](https://miro.medium.com/max/1400/1*CsJ05WEGfunYMLGfsT2sXA.gif)
+""")
+df = pd.read_csv("https://api.covid19india.org/csv/latest/state_wise.csv")
+st.write(f"State wise data")
+st.write(df)
