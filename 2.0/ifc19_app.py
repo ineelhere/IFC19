346dd2ce4452ff409c75c56d01320995a3534759
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-08-15 07:17:47 +0530

    Update ifc19_app.py

--- 2.0/ifc19_app.py
+++ 2.0/ifc19_app.py
@@ -1,17 +1,24 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime
+import pytz
 
 st.sidebar.subheader("Look out here for more COVID related information")
 # st.markdown("![](https://komarev.com/ghpvc/?username=ineelhere&label=VISITOR+COUNTS)")
 st.title("India Fights COVID19 (IFC19)")
 st.markdown("""
-webapp version 2.0 | development under progress
+webapp version 2.0 | **Currently under development**
 ![](https://miro.medium.com/max/1400/1*CsJ05WEGfunYMLGfsT2sXA.gif)
 """)
 df = pd.read_csv("https://api.covid19india.org/csv/latest/state_wise.csv")
+
+
+IST = pytz.timezone('Asia/Kolkata') 
+datetime_ist = datetime.now(IST)
+st.write(f'**🔄 Data was last updated on: `{df.loc[df.State=="Total"]["Last_Updated_Time"][0]}` IST  \n🕖 Current time: `{datetime_ist.strftime("%d/%m/%Y %H:%M:%S %Z")}` IST**')
+
 st.write(f"State wise data")
 st.write(df)
-
 st.markdown("""
 ___
 **© [Indraneel Chakraborty](https://www.linkedin.com/in/indraneelchakraborty/) | 2021**
