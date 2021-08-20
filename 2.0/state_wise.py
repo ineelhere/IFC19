b0bd76c37bfbc56879bb7474bd91d3a5f662def8
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-08-20 07:42:46 +0530

    Update state_wise.py

--- 2.0/state_wise.py
+++ 2.0/state_wise.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import pandas as pd
 import datetime
+import plotly.express as px
 
 def state_wise():
     df = pd.read_csv("https://api.covid19india.org/csv/latest/states.csv")
     df_all = df.loc[df["Date"]==df.Date[len(df)-1]]
     df_all = df_all.loc[df.State!="India"]
     df_all = df_all[["State", "Confirmed", "Recovered", "Deceased"]].reset_index(drop=True) 
-    st.write(f"**Numbers in the Indian States as of `{datetime.datetime.strptime(df.Date[len(df)-1], '%Y-%m-%d').strftime('%d %B %Y')}`**")
+    st.write(f"**Numbers (cumulative) in the Indian States as of `{datetime.datetime.strptime(df.Date[len(df)-1], '%Y-%m-%d').strftime('%d %B %Y')}`**")
     st.dataframe(df_all.set_index("State"))
-    st.line_chart(df_all.set_index("State"))
+    df_c = df_all[["State", "Confirmed"]].sort_values(by="Confirmed", ascending=False)
+    fig = px.pie(df_c, values='Confirmed', names='State', title='Confirmed COVID19 Cases in Indian States')
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+    st.plotly_chart(fig, use_container_width=True)
