8300c6a080f625911b6d87221ae0af2217dd2d16
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-08-20 07:01:45 +0530

    Update state_wise.py

--- 2.0/state_wise.py
+++ 2.0/state_wise.py
@@ -9,3 +9,4 @@ def state_wise():
     df_all = df_all[["State", "Confirmed", "Recovered", "Deceased"]].reset_index(drop=True) 
     st.write(f"**Numbers in the Indian States as of `{datetime.datetime.strptime(df.Date[len(df)-1], '%Y-%m-%d').strftime('%d %B %Y')}`**")
     st.dataframe(df_all.set_index("State"))
+    st.line_chart(df_all.set_index("State"))
