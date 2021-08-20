4bb6ee372236fb4fff3375e844b5d66dc7f3e783
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-08-20 08:37:49 +0530

    Update state_wise.py

--- 2.0/state_wise.py
+++ 2.0/state_wise.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import datetime
 import plotly.express as px
+import base64
 
 def state_wise():
     df = pd.read_csv("https://api.covid19india.org/csv/latest/states.csv")
@@ -10,6 +11,10 @@ def state_wise():
     df_all = df_all[["State", "Confirmed", "Recovered", "Deceased"]].reset_index(drop=True) 
     st.write(f"**Numbers (cumulative) in the Indian States as of `{datetime.datetime.strptime(df.Date[len(df)-1], '%Y-%m-%d').strftime('%d %B %Y')}`**")
     st.dataframe(df_all.set_index("State"))
+    csv = df_all.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}">Download the above as a CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+    st.markdown(href, unsafe_allow_html=True)
     df_c = df_all[["State", "Confirmed"]].sort_values(by="Confirmed", ascending=False)
     fig = px.pie(df_c, values='Confirmed', names='State', title='Confirmed COVID19 Cases in Indian States')
     fig.update_traces(textposition='inside', textinfo='percent+label')
