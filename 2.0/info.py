d4a89389bcfa134af46893d3d8f8d818617e65e4
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-08-21 11:16:09 +0530

    Update info.py

--- 2.0/info.py
+++ 2.0/info.py
@@ -1,7 +1,45 @@
 import streamlit as st
 
 def resources():
-    html_text = """
-    <a class="twitter-timeline" href="https://twitter.com/CovidIndiaSeva?ref_src=twsrc%5Etfw">Tweets by CovidIndiaSeva</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-    """
-    st.markdown(html_text, unsafe_allow_html=True)
+   st.markdown("""
+    * http://www.arogya.maharashtra.gov.in
+    * http://www.assam.gov.in
+    * http://www.cghealth.nic.in
+    * http://www.chdcovid19.in
+    * http://www.collectorate.py.gov.in
+    * http://www.covid.icmr.org.in
+    * http://www.covid.ladakh.gov.in
+    * http://www.covid19.itanagarsmartcity.in
+    * http://www.covid19.karnataka.gov.in
+    * http://www.covid19.telangana.gov.in
+    * http://www.dashboard.kerala.gov.in
+    * http://www.delhifightscorona.in
+    * http://www.dhs.kerala.gov.in
+    * http://www.dronamaps.com
+    * http://www.facebook.com
+    * http://www.goa.gov.in
+    * http://www.gujcovid19.gujarat.gov.in
+    * http://www.health.mizoram.gov.in
+    * http://www.health.odisha.gov.in
+    * http://www.health.uk.gov.in
+    * http://www.hmfw.ap.gov.in
+    * http://www.icmr.nic.in
+    * http://www.karunadu.karnataka.gov.in
+    * http://www.mahe.gov.in
+    * http://www.meghalayaonline.gov.in
+    * http://www.mobile.twitter.com
+    * http://www.mohfw.gov.in
+    * http://www.mphealthresponse.nhmmp.gov.in
+    * http://www.mpinfo.org
+    * http://www.nhmharyana.gov.in
+    * http://www.nrhmhp.gov.in
+    * http://www.nrhmmanipur.org
+    * http://www.pbhealth.gov.in
+    * http://www.phdmah.maps.arcgis.com
+    * http://www.rajswasthya.nic.in
+    * http://www.stopcoronatn.in
+    * http://www.twitter.com
+    * http://www.wbhealth.gov.inwbhealth.gov.in
+
+   
+   """)
