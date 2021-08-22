37bc723c2dbe5e82be6c1950b8ae1cb8d54a8502
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2021-08-22 12:50:24 +0530

    Update vaccination.py

--- 2.0/vaccination.py
+++ 2.0/vaccination.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import base64
 
 def vaccination():
     df_vac = pd.read_csv("http://api.covid19india.org/csv/latest/cowin_vaccine_data_statewise.csv")
@@ -14,6 +15,11 @@ def vaccination():
     st.write(f"**Latest data available on COVID19 vaccination in India** ")
     value = slider_ph.slider("Move the slider to list data for the last 'n' number of days", 1, len(df_vac), 7, 1)
     st.dataframe(df_vac.head(value).set_index(["Updated On"]))
+    
+    csv = (df_vac.to_csv(index=False))
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}">Download the above as a CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+    st.markdown(href, unsafe_allow_html=True)
 
     df_long=pd.melt(df_vac.iloc[::-1].reset_index(), id_vars=['Updated On'], value_vars=['Total Doses Administered', 'Sessions', 'First Dose Administered', 'Second Dose Administered'])
     fig = px.line(df_long, x='Updated On', y='value', color='variable')
@@ -24,3 +30,5 @@ def vaccination():
     fig = px.line(df_long, x='Updated On', y='value', color='variable')
     fig.update_layout(hovermode='x')
     st.plotly_chart(fig, use_container_width=True)
+
+    st.error("**GET YOURSELF FULLY VACCINATED TODAY. DO NOT DELAY.**  \nVisit - https://www.cowin.gov.in/")
